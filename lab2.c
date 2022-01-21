16a8837821138432e065dd38bb75142a2c27a32c
HungYi Lu <kevin@HungYide-MacBook-Pro.local>
2022-01-21 09:34:57 -0600

    submit

---
+++ lab2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Sort(int arr[], int N){
+
+    int temp, currLoc, i;
+	for (i=1; i<N; i++){
+	    currLoc = i;
+	    while (currLoc > 0 && arr[currLoc-1] > arr[currLoc]){
+		    temp = arr[currLoc];
+		    arr[currLoc] = arr[currLoc-1];
+		    arr[currLoc-1] = temp;
+		    currLoc--;
+	    }
+	}
+}
+
+int main(){
+
+    int N;
+
+    printf("Enter the Number of Array Elements: ");
+    scanf("%d", &N);
+
+    int array[N];
+    int i = 0;
+    int num = 0;
+
+    for(i=0;i<N;i++){
+        printf("Number %d of the Array: ", i+1 );
+        scanf("%d", &num);
+        array[i] = num;
+    }
+
+    printf("Given Array is: ");
+    for(i=0;i<N;i++){
+        printf("%d, ", array[i]);
+    }
+    printf("\n");
+
+    Sort(array, N);
+
+    printf("Sorted Array is: ");
+    for(i=0;i<N;i++){
+        printf("%d, ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
