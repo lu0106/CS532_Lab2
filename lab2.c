b83c40cff1966d09205f93ead7666056399eb0c1
HungYi Lu <kevin@HungYide-MacBook-Pro.local>
2022-01-21 09:38:41 -0600

    add commit

--- lab2.c
+++ lab2.c
@@ -1,3 +1,8 @@
+// # CS532_Lab2
+// # GitHub lu0106
+// # SSH: git@github.com:lu0106/CS532_Lab2.git
+// # HTTPS: https://github.com/lu0106/CS532_Lab2.git
+
 #include <stdio.h>
 #include <stdlib.h>
 
